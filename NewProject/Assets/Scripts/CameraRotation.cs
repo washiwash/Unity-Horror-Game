916c3d34e75da5440c3082e1ee48ed0b2026a74d
Zycrus <phantom.bot.202@gmail.com>
2022-09-28 14:21:53 +0800

    Added Comments

--- NewProject/Assets/Scripts/CameraRotation.cs
+++ NewProject/Assets/Scripts/CameraRotation.cs
@@ -23,13 +23,11 @@ public class CameraRotation : MonoBehaviour
         mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens.x,
                                     Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens.y);
 
-        //rotation.y += mouseDelta.x;
+        // Player Horizontal Rotation
         playerTransform.Rotate(Vector3.up * mouseDelta.x);
 
+        // Camera Vertical Rotation
         rotation.x = Mathf.Clamp(rotation.x - mouseDelta.y, -85f, 85f);
         transform.localEulerAngles = Vector3.right * rotation.x;
-
-        //playerTransform.rotation = Quaternion.Euler(0, rotation.y, 0);
-        //transform.rotation = Quaternion.Euler(rotation.x, playerTransform.rotation.y, 0);
     }
 }
