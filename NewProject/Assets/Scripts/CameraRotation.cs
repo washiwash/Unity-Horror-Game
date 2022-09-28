34d7d91c8e05af56493a8292b887047d6e51e503
Zycrus <phantom.bot.202@gmail.com>
2022-09-28 14:18:41 +0800

    Added Camera Controls

--- NewProject/Assets/Scripts/CameraRotation.cs
+++ NewProject/Assets/Scripts/CameraRotation.cs
@@ -7,9 +7,10 @@ public class CameraRotation : MonoBehaviour
     [Header("Rotation")]
     [SerializeField] private Vector2 sens = new Vector2(400f, 400f);
     [SerializeField] private Vector2 rotation;
+    [SerializeField] private Transform playerTransform;
 
     [Header("Mouse")]
-    [SerializeField] private Vector2 mousePosition;
+    [SerializeField] private Vector2 mouseDelta;
 
     private void Start()
     {
@@ -19,12 +20,16 @@ public class CameraRotation : MonoBehaviour
 
     private void Update()
     {
-        mousePosition = new Vector2(Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens.x,
+        mouseDelta = new Vector2(Input.GetAxisRaw("Mouse X") * Time.deltaTime * sens.x,
                                     Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sens.y);
 
-        rotation.y += mousePosition.x;
-        rotation.x = Mathf.Clamp(rotation.x - mousePosition.y, -85f, 85f);
+        //rotation.y += mouseDelta.x;
+        playerTransform.Rotate(Vector3.up * mouseDelta.x);
 
-        transform.rotation = Quaternion.Euler(rotation.x, rotation.y, 0);
+        rotation.x = Mathf.Clamp(rotation.x - mouseDelta.y, -85f, 85f);
+        transform.localEulerAngles = Vector3.right * rotation.x;
+
+        //playerTransform.rotation = Quaternion.Euler(0, rotation.y, 0);
+        //transform.rotation = Quaternion.Euler(rotation.x, playerTransform.rotation.y, 0);
     }
 }
