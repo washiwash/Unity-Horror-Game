855adf667fd81e8f7d1aeff33eac85e454dd4380
Zycrus <phantom.bot.202@gmail.com>
2022-09-28 15:42:44 +0800

    Adjusted PlayerMovement

--- NewProject/Assets/Scripts/PlayerMovement.cs
+++ NewProject/Assets/Scripts/PlayerMovement.cs
@@ -4,11 +4,11 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float walkSpeed = 10f;
-    private CharacterController control;
+    private Rigidbody body;
 
     private void Start()
     {
-        control = GetComponent<CharacterController>();
+        body = GetComponent<Rigidbody>();
     }
 
     private void Update()
@@ -26,6 +26,6 @@ public class PlayerMovement : MonoBehaviour
         Vector2 inputDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         Vector3 velocity = (transform.forward * inputDir.y + transform.right * inputDir.x) * walkSpeed;
 
-        control.Move(velocity * Time.deltaTime);
+        body.velocity = new Vector3(velocity.x, body.velocity.y, velocity.z);
     }
 }
