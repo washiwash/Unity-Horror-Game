2bebf33893f139f6a10abd50d600307760661038
Zycrus <47321670+zycrus@users.noreply.github.com>
2022-09-28 11:18:01 +0800

    Update CameraMovement.cs
    Changed 'MoveCamera' class name to 'CameraMovement'

--- 2nd Horror Game/Assets/Scripts/CameraMovement.cs
+++ 2nd Horror Game/Assets/Scripts/CameraMovement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveCamera : MonoBehaviour
+public class CameraMovement : MonoBehaviour
 {
     public Transform cameraPosition;
 
