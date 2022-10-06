75f7e3e1f32bc593eaf136adc0d33a2d7eadc200
Zycrus <phantom.bot.202@gmail.com>
2022-10-06 09:59:27 +0800

    Item Script

---
+++ NewProject/Assets/Scripts/Item.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("Item Details")]
+    public string type;
+    public int amount = 1;
+}
