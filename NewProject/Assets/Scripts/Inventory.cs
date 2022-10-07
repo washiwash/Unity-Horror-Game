fea3f30480d5e337487ff62b5f3bf3f8590180aa
Zycrus <phantom.bot.202@gmail.com>
2022-10-07 08:20:05 +0800

    Added function for fixing animatronics

--- NewProject/Assets/Scripts/Inventory.cs
+++ NewProject/Assets/Scripts/Inventory.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Inventory : MonoBehaviour
 {
     [SerializeField] private Camera cam;
     [SerializeField] private float range = 10f;
+
+    [Header("UI")]
+    [SerializeField] private TextMeshProUGUI pickUpText;
 
     [Header("Keybids")]
     [SerializeField] private KeyCode keyInteract = KeyCode.E;
@@ -39,8 +43,59 @@ public class Inventory : MonoBehaviour
                     item.gameObject.SetActive(false);
                     Debug.Log("Collected" + item.amount.ToString() + " " + item.type + ": Total is " + itemDict[item.type].ToString());
                 }
+                pickUpText.text = "Press '" + keyInteract.ToString() + "' to pick up";
+                pickUpText.enabled = true;
+            }
+            else if (hit.transform.tag == "Animatronic")
+            {
+                Animatronic anima = hit.transform.GetComponent<Animatronic>();
+                Animator animaAnim = hit.transform.GetComponent<Animator>();
+                List<Animatronic.ItemStruct> itemsNeeded = anima.itemsNeeded;
+                
+                if (IsInInventory(itemsNeeded[0].type) && itemDict[itemsNeeded[0].type] >= itemsNeeded[0].amount)
+                {
+                    pickUpText.text = "Press " + keyInteract.ToString() + " to fix";
+                    pickUpText.color = Color.green;
+
+                    if (Input.GetKeyDown(keyInteract))
+                    {
+                        FixAnima(animaAnim);
+                        foreach (Animatronic.ItemStruct item in itemsNeeded)
+                        {
+                            itemDict[item.type] -= 1;
+                            Debug.Log(itemDict[item.type]);
+                            if (itemDict[item.type] <= 0)
+                            {
+                                itemDict.Remove(item.type);
+                                items.Remove(item.type);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    pickUpText.text = itemsNeeded[0].amount.ToString() + " " + itemsNeeded[0].type + " needed";
+                    pickUpText.color = Color.white;
+                }
+                if (animaAnim.GetBool("Fixed"))
+                    pickUpText.text = "Fixed";
+
+                pickUpText.enabled = true;
+            }
+            else
+            {
+                pickUpText.enabled = false;
             }
         }
+        else
+        {
+            pickUpText.enabled = false;
+        }
+    }
+
+    private void FixAnima(Animator animaAnim)
+    {
+        animaAnim.SetBool("Fixed", true);
     }
 
     private bool IsInInventory(string itemType)
